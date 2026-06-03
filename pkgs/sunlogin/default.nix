bb17f50955cc75af6aa5414f7e03e91de7ba2a10
LMQ00 <1536443400@qq.com>
2026-06-03 23:03:06 +0800

    Try list format for autoPatchelfHookArgs

--- pkgs/sunlogin/default.nix
+++ pkgs/sunlogin/default.nix
@@ -73,7 +73,7 @@ stdenv.mkDerivation rec {
     makeWrapper
   ];
 
-  autoPatchelfHookArgs = "--ignore-missing=libwidevinecdm.so --ignore-missing=libgconf-2.so.4 --ignore-missing=libcrypt.so.1";
+  autoPatchelfHookArgs = [ "--ignore-missing" "libwidevinecdm.so" "libgconf-2.so.4" "libcrypt.so.1" ];
 
   buildInputs = [
     libappindicator-gtk3
