4f788da0ff20224f9771b12803ea749e48c94fa3
LMQ00 <1536443400@qq.com>
2026-07-07 20:13:20 +0800

    hanako: switch to X11 rendering path to fix settings page lag

--- pkgs/hanako/default.nix
+++ pkgs/hanako/default.nix
@@ -150,9 +150,7 @@ stdenv.mkDerivation {
     makeWrapper $out/lib/hanako/hanako $out/bin/.hanako-wrapped \
       --add-flags "--no-sandbox" \
       --set CHROME_SANDBOX "$out/lib/hanako/chrome-sandbox" \
-      --set ELECTRON_OZONE_PLATFORM_HINT "auto" \
-      --add-flags "--enable-features=UseOzonePlatform,WaylandWindowDecorations" \
-      --add-flags "--disable-gpu-compositing" \
+      --set ELECTRON_OZONE_PLATFORM_HINT "x11" \
       --prefix LD_LIBRARY_PATH : "$out/lib/hanako" \
       --prefix LD_LIBRARY_PATH : "/run/opengl-driver/lib" \
       --set LIBGL_DRIVERS_PATH "/run/opengl-driver/lib/dri"
